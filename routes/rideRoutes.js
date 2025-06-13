42a2c9c78cdda8b627b5b8e1a88cf897462e9cbc
Rafia Malik <122554649+rafiya618@users.noreply.github.com>
2025-06-14 00:41:48 +0500

    Update rideRoutes.js

--- routes/rideRoutes.js
+++ routes/rideRoutes.js
@@ -1,10 +1,10 @@
-// routes/rideRoutes.js
 import express from 'express';
-import { createRide,getUserRideHistory,endRide} from '../controllers/rideController.js';
+import { createRide, getUserRideHistory, endRide, cancelRide } from '../controllers/rideController.js';
 import authMiddleware from '../middleware/authMiddleware.js';
 
 const router = express.Router();
-router.post('/create', createRide); 
+router.post('/create', authMiddleware, createRide); 
 router.get('/ride-history', authMiddleware, getUserRideHistory);
 router.put('/end/:rideId', authMiddleware, endRide);
+router.put('/cancel/:rideId', authMiddleware, cancelRide); 
 export default router;
